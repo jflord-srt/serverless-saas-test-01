b56c6879f76c1776b3c6e632dd017cd1f5d125f8
Jean Lord <jean.lord@silkroad.com>
2022-11-07 10:09:57 -0700

    GitHub action

--- build.ps1
+++ build.ps1
@@ -1,18 +1,18 @@
-# dotnet build --configuration Release "$PSScriptRoot\services\TenantManagement\TenantManagement.sln"
+dotnet build --configuration Release "$PSScriptRoot\services\TenantManagement\TenantManagement.sln"
 
-# cd "$PSScriptRoot\apps\client-ux\"
-# npm install
-# npm run build
-# cd "$PSScriptRoot"
+cd "$PSScriptRoot\apps\client-ux\"
+npm install
+npm run build
+cd "$PSScriptRoot"
 
-# cd "$PSScriptRoot\apps\saas-operations-ux\"
-# npm install
-# npm run build
-# cd "$PSScriptRoot"
+cd "$PSScriptRoot\apps\saas-operations-ux\"
+npm install
+npm run build
+cd "$PSScriptRoot"
 
 cd "$PSScriptRoot\cdk-resources\publish-sns-message-function\"
 npm install
 npm run build
 cd "$PSScriptRoot"
 
-# mvn -f "$PSScriptRoot\cdk-resources\deploy-db-lambda-function\pom.xml" clean install compile assembly:single
+mvn -f "$PSScriptRoot\cdk-resources\deploy-db-lambda-function\pom.xml" clean install compile assembly:single
